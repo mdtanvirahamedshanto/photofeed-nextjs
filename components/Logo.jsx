aee9b3f71c4955e8d921ec4c21db7ccba4e2a25c
mdtanvirahamedshanto <mdtanvirahamdsanto987@gmail.com>
2024-05-08 22:43:50 +0600

    chore: Update Logo and Navbar components

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,18 +1,18 @@
-import Link from "next/link"
-import Image from "next/image"
+import Image from "next/image";
+import Link from "next/link";
 
 const Logo = () => {
   return (
     <Link href="/">
       <Image
         className="max-w-[100px] md:max-w-[165px]"
-        src="/lws_logo.png"
+        src="/mylogo.png"
         alt="Lws"
         height={100}
         width={165}
       />
     </Link>
-  )
-}
+  );
+};
 
-export default Logo
+export default Logo;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,14 +1,13 @@
 import Logo from "./Logo";
 
 const Navbar = () => {
-    return (
-        <nav className="py-4 md:py-6 border-b">
-            <div className="container mx-auto flex items-center justify-between gap-x-6">
-                <Logo />
-            </div>
-
-        </nav>
-    );
+  return (
+    <nav className="py-2 md:py-4 border-b">
+      <div className="container h-[50px]  flex items-center justify-between gap-x-6">
+        <Logo />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
